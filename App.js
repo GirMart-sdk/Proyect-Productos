321626249780e53ad88d1b0f51148b380cae9a8e
Jamilton Girón Martínez <jamil0422@outlook.es>
2022-09-06 21:14:30 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+class product
+{
+  constructor(name, price, year)
+  {
+    this.name = name;
+    this.price = price;
+    this.year = year;
+  }
+}
+
+class UI
+{
+  addProduct ()
+  {
+  }
+  deleteProduct ()
+  {
+  }
+  showMessage ()
+  {
+  }
+}
+
+// DOM Events
+
+document.getElementById('product-form')
+addEventListener('sumit', function (e)
+{
+
+})
